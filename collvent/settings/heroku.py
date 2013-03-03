04afcb0358e880920f3e2cc995087a366617e2ee
Andy Nguyen <anguyenhuy@gmail.com>
2013-03-02 16:06:26 -0800

    updated email settings for production environments

--- collvent/settings/heroku.py
+++ collvent/settings/heroku.py
@@ -9,6 +9,9 @@ DATABASES = {
 }
 
 # Sendgrid config
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 
