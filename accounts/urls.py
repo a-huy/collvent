3ff1774cb86b230296dd44dcdb49445be1a4cdee
Mike Wilson <computergeekxp@gmail.com>
2013-03-03 23:23:06 -0800

    Added /accounts/login url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('accounts.views',
     url(r'^create/$', 'create_account', name='create_account'),
+    url(r'^login/$', 'login'),
 )
