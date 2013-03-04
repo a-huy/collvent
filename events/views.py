93c994d5e95ded4a5ed0f92647d76d1d3cc1b45a
Mike Wilson <computergeekxp@gmail.com>
2013-03-04 04:19:25 -0800

    Added footer.

--- events/views.py
+++ events/views.py
@@ -18,7 +18,6 @@ def create_event(request):
 def list_events(request):
     template_vars = {}
     events = request.user.getEvents()
-    print events
     for event in events:
         status = request.user.getEventRsvp(event)
         event.rsvp_status = status
